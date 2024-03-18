886ba1b18d4976fc3b6f7e45af75fc7da1a83d27
muhammed-jahas <jahaspty1@gmail.com>
2024-03-18 13:37:07 +0530

    fix: change in network url

--- lib/core/network/network_url.dart
+++ lib/core/network/network_url.dart
@@ -2,7 +2,8 @@
 const baseUrl= 'https://trudd-server.onrender.com';
 const webSocketUrl ='ws://trudd-server.onrender.com';
 
-
+// const webSocketUrl = 'ws://10.4.3.252:3000'
+// const baseUrl = 'http://10.4.3.252:3000';
 
 const createSpotUrl = '$baseUrl/api/leader/createspot';
 const joinSpotUrl = '$baseUrl/api/user/joinspot';
